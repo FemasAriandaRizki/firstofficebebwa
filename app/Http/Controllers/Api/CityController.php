a459d9781d3607b7ca9d15a747bd7f754991cf76
femas arianda <femasariandarizki@students.undip.ac.id>
2024-12-01 11:57:18 +0700

    fixing routing endpoint API

--- app/Http/Controllers/Api/CityController.php
+++ app/Http/Controllers/Api/CityController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\CityResource;
+use App\Models\City;
 use Illuminate\Http\Request;
 
 class CityController extends Controller
 {
-    //
     public function index()
     {
         $cities = City::withCount('officeSpaces')->get();
--- app/Http/Controllers/Api/OfficeSpaceController.php
+++ app/Http/Controllers/Api/OfficeSpaceController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\OfficeSpaceResource;
+use App\Models\OfficeSpace;
 use Illuminate\Http\Request;
 
 class OfficeSpaceController extends Controller
 {
-    //
     public function index()
     {
         $officeSpaces = OfficeSpace::with(['city'])->get();
